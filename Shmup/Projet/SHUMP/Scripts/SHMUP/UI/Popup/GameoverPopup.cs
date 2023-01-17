95593c74399a2f6adb8a9ae53ebc3c25cd4cac11
Lefevre-Florian <lefevreflorian18@gmail.com>
2023-01-17 14:47:32 +0100

    Gameover popup

--- Shmup/Projet/SHUMP/Scripts/SHMUP/UI/Popup/GameoverPopup.cs
+++ Shmup/Projet/SHUMP/Scripts/SHMUP/UI/Popup/GameoverPopup.cs
@@ -1,6 +1,6 @@
-using Com.IsartDigital.SHMUP.Structure;
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace Com.IsartDigital.SHMUP.UI {
 
@@ -8,18 +8,42 @@ namespace Com.IsartDigital.SHMUP.UI {
 	{
 
 		[Export] private NodePath scoreLabelPath = default;
+		[Export] private List<int> scorePhase = new List<int>();
+		[Export] private NodePath xpProgressPath = default;
+		[Export] private float duration;
+		[Export] private Tween.TransitionType transitionType = default;
+		[Export] private Tween.EaseType easeType = default;
 
-		Hud hud = null;
+		private const string PROPERTY_VALUE = "value";
+
+		private Hud hud = null;
+		private ProgressBar xpProgress = null;
+
+		private Label scoreLabel = null;
+		private int scoreIndex = 0;
 
 		public override void _Ready()
 		{
 			base._Ready();
 			hud = Hud.GetInstance();
 
-			Label lLabel = GetNode<Label>(scoreLabelPath);
-			lLabel.Text = hud.GetScoreValue().ToString();
+			scoreLabel = GetNode<Label>(scoreLabelPath);
+
+			xpProgress = GetNode<ProgressBar>(xpProgressPath);
+
+			SceneTreeTween lTween = GetTree().CreateTween();
+			lTween.TweenMethod(this, nameof(WriteScore), 0, hud.GetScoreValue(), duration)
+				  .SetTrans(transitionType)
+				  .SetEase(easeType);
+			lTween.Play();
 		}
 
-	}
+		private void WriteScore(int pValue)
+        {
+			GD.Print(pValue);
+			scoreLabel.Text = pValue.ToString();
+        }
+
+    }
 
 }
